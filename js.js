308f80d1db992d88dc24feeac3f5b81def78ad51
OlesiaPonomarenko <160940182+OlesiaPonomarenko@users.noreply.github.com>
2025-12-01 05:22:37 +0200

    new18

--- js.js
+++ js.js
@@ -20,3 +20,67 @@ backBtn.addEventListener('click', ()=> {
      }
     document.querySelector('#picture').src = img[i];
 })
+
+/*
+
+const textEl = document.querySelectorAll('.divPEl');
+
+function removeFocus () {
+    textEl.forEach(el => el.classList.remove('selected'));
+}
+
+textEl.forEach( divPEl => {
+    el.addEventListener('mouseover', () =>{
+    removeFocus();
+    el.classList.add('selected');
+    })
+    removeFocus = () => {
+        divPEl.forEach(divPEl => {
+            divPEl.classList.remove('selected');
+        })
+    }
+})
+
+textEl.forEach( divPEl => {
+    divPEl.addEventListener ('click', () => {
+        removeFocus();
+        divPEl.classList.add('selected');
+    })
+})
+
+*/
+
+
+const textEls = document.querySelectorAll('.divPEl');
+
+function removeFocus() {
+  textEls.forEach(el => el.classList.remove('selected'));
+}
+
+textEls.forEach(el => {
+  el.addEventListener('mouseover', () => {
+    removeFocus();
+    el.classList.add('selected');
+  });
+  el.addEventListener('click', () => {
+    removeFocus();
+    el.classList.add('selected');
+  });
+  el.addEventListener('mouseout', () => {
+     el.classList.remove('selected');
+  });
+});
+
+function randomPastel() {
+    const r = Math.floor(Math.random() * 127 + 127);
+    const g = Math.floor(Math.random() * 127 + 127);
+    const b = Math.floor(Math.random() * 127 + 127);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+textEls.forEach(div => {
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = randomPastel();
+    });
+});
+
